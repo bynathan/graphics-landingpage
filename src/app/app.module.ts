f8f7ed6ab62d8c1fbf597c54427e9d71147f23d7
Nathan Silva <nathancontato14112003@gmail.com>
2023-10-06 17:07:31 -0300

    select inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +9,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { CalculateComponent } from './pages/calculate/calculate.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,11 @@ import { HeaderComponent } from './shared/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TypeaheadModule.forRoot(),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/calculate/calculate.component.ts
+++ src/app/pages/calculate/calculate.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormControl, UntypedFormGroup, FormControl, FormGroup } from '@angular/forms';
+import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-calculate',
@@ -11,4 +12,45 @@ export class CalculateComponent implements OnInit{
     document.getElementsByTagName('body')[0].classList.add('calculate-page');
     window.scrollTo({top: 0, behavior: 'smooth'});
   }
+
+  typesCtrl = new FormControl('');
+
+  typeForm = new FormGroup({
+    type: this.typesCtrl
+  });
+
+  types = [
+    'Residencial',
+    'Empresarial',
+    'Industrial',
+  ];
+
+  munisCtrl = new FormControl('');
+
+  muniForm = new FormGroup({
+    muni: this.munisCtrl
+  });
+
+  munis = [
+    'Rio de Janeiro',
+    'São Gonçalo',
+    'Duque de Caxias',
+    'Nova Iguaçu', 
+    'Niterói', 
+    'Belford Roxo', 
+    'São João de Meriti', 
+    'Campos dos Goytacazes', 
+    'Petrópolis', 
+    'Volta Redonda', 
+    'Magé', 
+    'Itaboraí', 
+    'Angra dos Reis', 
+    'Macaé', 
+    'Teresópolis', 
+    'Cabo Frio', 
+    'Nova Friburgo', 
+    'Mesquita', 
+    'Barra Mansa', 
+    'Nilópolis'
+  ];
 }
